c50f999fa281fcda5908c49fb3a889b3d3e75a6a
Marti <marti.sabater@estudiantat.upc.edu>
2026-02-21 09:16:36 +0100

    Exercici Acabat

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,36 @@
 public static void main(String[] args) {
-    Figura[] v = new Figura[][4];
+    Figura[] v = new Figura[4];
 
     v[0] = new Rectangle(5, 3);
     v[1] = new Circle(5);
-    v[2] = new Square(5);
+    v[2] = new Quadrat(5);
     v[3] = new Circle(120);
 
-    double res = sum(v);
+    double res = suma(v);
 
     System.out.println("sum: " + res);
 
-    print(v);
+   imprimir(v);
 
-    sort(v);
+    ordena(v);
 
-    print(v);
+    imprimir(v);
+}
+
+private static void imprimir(Figura[] v) {
+    System.out.println(v);
+}
+
+private static void ordena(Figura[] v) {
+    Arrays.sort(v);
+}
+
+private static double suma(Figura[] v) {
+    double res = 0;
+
+    for (Figura f: v) {
+        res+=f.area();
+    }
+
+    return res;
 }
